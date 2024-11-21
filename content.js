e72b5a3bb90265b010bbabb1061631cf22f1c930
artIlluminati <artilluminatigd@gmail.com>
2024-11-21 21:15:20 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,28 @@
+// Массив с селекторами для скрытия блоков
+const selectorsToHide = [
+    "div#dismissible.style-scope.ytd-rich-shelf-renderer",
+    "#dismissible.style-scope ytd-rich-shelf-renderer",
+    // Селектор для раздела Shorts
+    // Добавляйте сюда другие селекторы, которые нужно скрывать
+    // Например:
+    // 'ytd-compact-video-renderer.style-scope.ytd-rich-grid-media', // Для скрытия других блоков
+];
+
+// Функция для скрытия элементов по селекторам
+const hideElements = () => {
+    selectorsToHide.forEach((selector) => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach((element) => {
+            element.style.display = "none";
+        });
+    });
+};
+
+// Ожидание загрузки контента на странице
+window.addEventListener("load", () => {
+    hideElements(); // Скрыть сразу при загрузке страницы
+
+    // Добавить наблюдатель для динамически добавляющихся элементов
+    const observer = new MutationObserver(hideElements);
+    observer.observe(document.body, { childList: true, subtree: true });
+});
